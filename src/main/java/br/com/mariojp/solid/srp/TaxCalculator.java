e6c9535314940440f6d2715808aeb3fa17e4ddde
Felicissimo777 <guilherme.felicissimo03@outlook.com>
2025-08-27 00:05:37 -0300

    mvn-q test

---
+++ src/main/java/br/com/mariojp/solid/srp/TaxCalculator.java
@@ -0,0 +1,15 @@
+package br.com.mariojp.solid.srp;
+
+public class TaxCalculator{
+    private double taxRate;
+    public TaxCalculator(){
+        String taxRateProperty = System.getProperty("tax.rate","0.10");
+        this.taxRate = Double.parseDouble(taxRateProperty);
+    }
+    public double calculateTax(double amount){
+        return amount * taxRate;
+    }
+    public double getTaxRate(){
+        return taxRate;
+    }
+}
