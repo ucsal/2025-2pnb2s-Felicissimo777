1538313b5aa8ec7c147ea8fea1f6d21daf94abb0
Felicissimo777 <guilherme.felicissimo03@outlook.com>
2025-09-04 16:31:30 -0300

    classes refatoradas

--- src/main/java/br/com/mariojp/solid/srp/TaxCalculator.java
+++ src/main/java/br/com/mariojp/solid/srp/TaxCalculator.java
@@ -1,15 +1,8 @@
 package br.com.mariojp.solid.srp;
 
-public class TaxCalculator{
-    private double taxRate;
-    public TaxCalculator(){
-        String taxRateProperty = System.getProperty("tax.rate","0,08");
-        this.taxRate = Double.parseDouble(taxRateProperty);
-    }
-    public double calculateTax(double amount){
-        return amount * taxRate;
-    }
-    public double getTaxRate(){
-        return taxRate;
-    }
+public class TaxCalculator {
+   public double calculate(double subtotal) {
+      double taxRate = Double.parseDouble(System.getProperty("tax.rate"));
+      return subtotal * taxRate;
+   }
 }
